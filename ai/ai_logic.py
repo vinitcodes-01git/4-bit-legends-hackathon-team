e2029d93f6d71d875fe19a32b521b9d9c2f7ccea
Vinit01 <219628266+vinitcodes-01git@users.noreply.github.com>
2026-03-25 10:04:04 +0530

    AI initial analyze function

---
+++ ai/ai_logic.py
@@ -0,0 +1,9 @@
+def analyze(data):
+    return {
+        "traffic": "Low",
+        "route": ["A", "B"],
+        "signal_time": 30,
+        "mode": "Normal",
+        "confidence": 0.7,
+        "reason": "Initial test"
+    }
